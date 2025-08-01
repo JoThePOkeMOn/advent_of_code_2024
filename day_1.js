9c3f88c6e4d69ec278ac83d9869e40bf508ffebf
JoThePOkeMOn <joelmathewsamuel@gmail.com>
2025-08-01 23:27:59 +0530

    1st one solves

--- day_1.js
+++ day_1.js
@@ -2,6 +2,10 @@ import fs from 'fs'
 let leftarray=[]
 let rightarray=[]
 let totalDistance=0
+let count = 0;
+let sum =0
+let mul;
+let total;
 fs.readFile("input.txt","utf8",(err,data)=>{
     const array = data.trim().split("\n").map((a)=> a.split("  "))
     for(let i =0 ; i<array.length; i++){
@@ -22,7 +26,20 @@ for(let i=0; i<leftarray.length;i++){
     totalDistance += Math.abs(leftarray[i]-rightarray[i])
 }
 // console.log("Does pairDifference contain any NaNs?", pairDifference.some(isNaN));
-console.log(totalDistance)
+// console.log(totalDistance)
+
+for(let i= 0;i < leftarray.length ; i++){
+    let leftelement = leftarray[i]
+    for (let j =0; j<rightarray.length;j++){
+        if(leftelement === rightarray[j]){
+            count++
+        }
+    }
+    mul = leftelement*count;
+    sum += mul
+    count =0
+}
+console.log(sum)
 })
 
- 
+
